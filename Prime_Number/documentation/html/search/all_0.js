540bdf1e7313a85942c73e5a55b7d5fa2d2fd193
stepin104880 <69037616+stepin104880@users.noreply.github.com>
2020-08-15 17:58:41 +0530

    Edited documentation file

---
+++ Prime_Number/documentation/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['prime_20number_20check_20by_20shekar_20jk_0',['Prime Number check by Shekar Jk',['../index.html',1,'']]],
+  ['primenumber_1',['primenumber',['../primenumber_8h.html#ae59cf1e971ce1cb907879921a85b28de',1,'primenumber.h']]],
+  ['primenumber_2eh_2',['primenumber.h',['../primenumber_8h.html',1,'']]]
+];
---
+++ Prime_Number/documentation/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['prime_20number_20check_20by_20shekar_20jk_5',['Prime Number check by Shekar Jk',['../index.html',1,'']]]
+];
